2cdab47b5af035c966aac8975f60b4106098f682
Zaky Syihab Hatmoko <zaky.syihab@gmail.com>
2022-08-07 22:21:54 +0700

    feat: AC-10 create collection detail page

--- src/pages/CollectionDetailPage/CollectionDetailPage.tsx
+++ src/pages/CollectionDetailPage/CollectionDetailPage.tsx
@@ -1,7 +1,109 @@
+/** @jsxImportSource @emotion/react */
+import AnimeCard from '../../components/Anime/AnimeCard'
+import { css } from '@emotion/react'
+import { COLORS, MQ } from '../../styles/Constants'
+import useLocalStorage, { LS_KEY } from '../../hooks/useLocalStorage'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { ANIME_DETAIL, AnimeDetailData, AnimeDetailVars } from '../../gql/queries'
+import { Collections } from '../../components/Collection/AddToCollectionButton'
+import { IoIosArrowBack } from 'react-icons/io'
+
+const HEADER_HEIGHT = 72
+
+const collectionDetailPageStyle = css({
+  overflowX: 'hidden',
+  paddingTop: `calc(${HEADER_HEIGHT}px + 0.25rem)`,
+  display: 'flex',
+  flexDirection: 'column',
+  paddingBottom: 72,
+  '.pagination': {
+    marginTop: '1.5rem',
+  },
+})
+
+const collectionDetailStyle = css({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+  gap: '1rem',
+  [MQ[1]]: {
+    gridTemplateColumns: 'repeat(3, minmax(0, 1fr))',
+  },
+  [MQ[2]]: {
+    gridTemplateColumns: 'repeat(5, minmax(0, 1fr))',
+  },
+})
+
+const emptyStyle = css({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: `calc(100vh - 2 * 1rem - ${HEADER_HEIGHT}px)`,
+  paddingBottom: 0,
+  '> span': {
+    textAlign: 'center',
+    fontSize: '1.75rem',
+    marginBottom: '1rem',
+  },
+})
+
+const headerStyle = css({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: '1.5rem',
+  fontWeight: 'bold',
+  backgroundColor: COLORS['dark-gray'],
+  height: HEADER_HEIGHT,
+  width: '100vw',
+  svg: {
+    cursor: 'pointer',
+    margin: '0 1rem',
+    '&:hover': {
+      filter: 'brightness(0.8)'
+    }
+  }
+})
+
 export default function CollectionDetailPage() {
+  const params = useParams()
+  const collectionId = params.collectionId!
+  const [collections] = useLocalStorage<Collections>(LS_KEY.COLLECTIONS, {})
+  const navigate = useNavigate()
+  const currentCollection = collections[collectionId]
+  const isEmpty = currentCollection.length < 1
+
   return (
-    <div>
-      CollectionDetailPage
-    </div>
+    <>
+      <header css={headerStyle}><IoIosArrowBack onClick={() => navigate('/collections')}/> {collectionId}</header>
+      <div css={[collectionDetailPageStyle, isEmpty && emptyStyle]}>
+        {isEmpty ?
+          <span>You haven't added any anime to this collection.</span>
+          :
+          <div css={collectionDetailStyle}>
+            {currentCollection.map(id => CollectionAnimeCard(id))}
+          </div>
+        }
+      </div>
+    </>
   )
+}
+
+function CollectionAnimeCard(id: number) {
+  const { data, error, loading } = useQuery<AnimeDetailData, AnimeDetailVars>(ANIME_DETAIL, { variables: { id } })
+  const navigate = useNavigate()
+
+  // TODO: Create Loading Component
+  if (loading) return <div>Loading ...</div>
+  // TODO: Create Error Component
+  if (error) return <div>{JSON.stringify(error)}</div>
+
+  return <AnimeCard
+    key={id}
+    imageUrl={data?.Media.coverImage.large || data?.Media.bannerImage}
+    title={data?.Media.title.romaji || ''}
+    onClick={() => navigate(`/animes/${id}`)}
+  />
 }
